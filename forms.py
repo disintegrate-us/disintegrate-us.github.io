4c48d1cbff281356ae2a5581ea5625057f2dffda
disintegrate-us <100065787+disintegrate-us@users.noreply.github.com>
2022-03-01 14:41:01 -0700

    Delete forms.py

--- forms.py
+++
@@ -1,7 +0,0 @@
-from wtforms import Form, PasswordField, SubmitField, validators
-
-
-class LoginPage_LoginForm(Form):
-    password = PasswordField(label="Password", validators=[validators.DataRequired()])
-    submit = SubmitField(label="Enter")
-
